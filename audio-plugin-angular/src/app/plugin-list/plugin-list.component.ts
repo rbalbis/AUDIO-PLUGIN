e8531992f8fd7e3c19d58bef0391c11267998038
rbalbis <balbis.rb@gmail.com>
2018-12-03 10:00:05 +0100

    ajout de la recherche par tag

--- audio-plugin-angular/src/app/plugin-list/plugin-list.component.ts
+++ audio-plugin-angular/src/app/plugin-list/plugin-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ElementRef } from "@angular/core";
 import { AngularFireDatabase, AngularFireObject } from "@angular/fire/database";
 import { Observable } from "rxjs";
+import { PluginAudio } from "../plugin-audio";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: "app-plugin-list",
@@ -9,7 +11,8 @@ import { Observable } from "rxjs";
 })
 export class PluginListComponent implements OnInit {
   value;
-  plugins = [1, 2, 3, 4, 5];
+  plugins = [];
+  srchTerm: string;
 
   itemRef: AngularFireObject<any>;
   item: Observable<any>;
@@ -20,20 +23,57 @@ export class PluginListComponent implements OnInit {
 
   ngOnInit() {
     this.getAllPlugin();
-  }
+      }
   ngAfterViewInit() {
     this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor =
       "#f6f6f6";
   }
 
-  getAllPlugin() {
-    var leadsRef = this.db.database.ref();
-    leadsRef.on("value", function(snapshot) {
-      snapshot.forEach(value => {
-        console.log("test");
+  onSearch(form: NgForm){
+    console.log(form.value.srchTerm);
+    
+    this.getFilteredPlugin(form.value.srchTerm));
+  }
 
-        console.log(value.val());
-      });
-    });
+  getFilteredPlugin(filtre : string){
+    this.plugins = [];
+    var ref = this.db.list('list-plugin',ref => ref.orderByChild('tag1').equalTo(filtre)).valueChanges().subscribe(
+      res => {
+        console.log(res);
+        res.forEach( elmt => {
+          this.plugins.push(<PluginAudio>elmt);
+        }
+
+        )
+      }
+    )
+
+    var ref = this.db.list('list-plugin',ref => ref.orderByChild('tag2').equalTo(filtre)).valueChanges().subscribe(
+      res => {
+        console.log(res);
+        res.forEach( elmt => {
+          this.plugins.push(<PluginAudio>elmt);
+        }
+
+        )
+      }
+    )
+
+  }
+
+  
+
+  getAllPlugin(){
+    this.db.list('list-plugin').valueChanges().subscribe(
+      res => {
+        console.log(res[0]);
+        res.forEach( (elmt) => {
+          this.plugins.push(<PluginAudio>elmt);
+        })
+        
+        console.log(this.plugins);
+        
+      }
+    )
   }
 }
